b0e755b7c87d7a074fc8ab67fa30fa43fa9715b9
felipeas314 <felipeas314@gmail.com>
2019-09-29 18:42:35 -0300

    implementaçao da lista de fotos

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+  photos: Photo[] = [];
+  filter: string = '';
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
+
+  ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+  }
+}
